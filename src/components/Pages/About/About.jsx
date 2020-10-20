41fff518f3210c56ea042774ab71300d4b7cc087
mr-bmv <mr.m.botkin@gmail.com>
2020-10-20 16:34:11 +0300

    BG About style

---
+++ src/components/Pages/About/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { AiFillYoutube } from 'react-icons/ai'
+import './About.css'
+
+const About = () => {
+    return (
+        <div className="about-wrapper">
+            <h2>Обо мне</h2>
+            <div className="card"><p>Меня зовут <span style={{ color: '#73a839' }}>Михаил Боткин</span></p>
+                <img src="../../../img/logo60.png" alt="" className="photo" />
+                <p>Я работал как <span style={{ color: '#73a839' }}>Software Developer </span>три года и теперь решил попробовать себя в Web разработке.
+                <p>Это приложение - мой первый самостоятельный проект.</p>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default About;
