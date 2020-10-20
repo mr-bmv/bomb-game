f064e5c2a28b6cf1cb85ff1fdb037f2b91e5e902
mr-bmv <mr.m.botkin@gmail.com>
2020-10-20 17:02:16 +0300

    BG About Clean UP

--- src/components/Pages/About/About.jsx
+++ src/components/Pages/About/About.jsx
@@ -7,7 +7,6 @@ const About = () => {
         <div className="about-wrapper">
             <h2>Обо мне</h2>
             <div className="card"><p>Меня зовут <span style={{ color: '#73a839' }}>Михаил Боткин</span></p>
-                <img src="../../../img/logo60.png" alt="" className="photo" />
                 <p>Я работал как <span style={{ color: '#73a839' }}>Software Developer </span>три года и теперь решил попробовать себя в Web разработке.
                 <p>Это приложение - мой первый самостоятельный проект.</p>
                 </p>
