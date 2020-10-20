67f2af420a208398dd85ad8d02e9e3112b6351e4
mr-bmv <mr.m.botkin@gmail.com>
2020-10-20 17:11:46 +0300

    BG Style cdd for NotFound page

---
+++ src/components/Pages/NotFound/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './NotFouns.css'
+
+const NotFound = () => {
+    return (
+        <div className='not-found'>
+            <h1>404</h1>
+            <div className="no">💣💣💣 А такой страницы нет 💣💣💣</div>
+        </div>
+    )
+}
+
+export default NotFound
