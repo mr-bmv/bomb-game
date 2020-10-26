7481036790a6a41054ad4ad4bc197d6e13f73cf6
mr-bmv <mr.m.botkin@gmail.com>
2020-10-26 17:51:00 +0300

    BG Fix Bug in About page

--- src/components/Pages/About/About.jsx
+++ src/components/Pages/About/About.jsx
@@ -6,9 +6,8 @@ const About = () => {
         <div className="about-wrapper">
             <h2>Обо мне</h2>
             <div className="card"><p>Меня зовут <span style={{ color: '#73a839' }}>Михаил Боткин</span></p>
-                <p>Я работал как <span style={{ color: '#73a839' }}>Software Developer </span>три года и теперь решил попробовать себя в Web разработке.
+                <p>Я работал как <span style={{ color: '#73a839' }}>Software Developer </span>три года и теперь решил попробовать себя в Web разработке.</p>
                 <p>Это приложение - мой первый самостоятельный проект.</p>
-                </p>
             </div>
         </div>
     )
