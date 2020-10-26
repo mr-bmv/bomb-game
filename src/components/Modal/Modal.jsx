2796201fce707738d6e42d53d6b5720e93790fa0
mr-bmv <mr.m.botkin@gmail.com>
2020-10-26 23:19:00 +0300

    BG Add animation for Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,74 +1,17 @@
 import React from 'react'
-import styled from 'styled-components';
+import { Spring } from 'react-spring/renderprops';
 import { useGameContext } from '../../context/GameContext';
 import { useModalContext } from '../../context/ModalContext';
+
+// components
 import { Loader } from '../Loader/Loader';
 import { Spinner } from '../Spinner/Spinner';
 
-import './Modal.css'
+// styles
+import { Background, ModalContent, ModalWrapper, WinText } from './ModalElements';
 
-const Background = styled.div`
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.8);
-  position: fixed;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ModalWrapper = styled.div`
-  // width: 300px;
-  // height: 300px;
-  box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
-  background: #fff;
-  color: #000;
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  position: relative;
-  z-index: 10;
-  border-radius: 10px;
-`;
-
-const ModalImg = styled.img`
-  width: 100%;
-  height: 100%;
-  border-radius: 10px 0 0 10px;
-  background: #000;
-`;
-
-const ModalContent = styled.div`
-  // display: flex;
-  // flex-direction: column;
-  grid-column: 2 /4;
-  justify-content: center;
-  align-items: center;
-  line-height: 1.8;
-  color: #141414;
-
-  p {
-    margin-bottom: 1rem;
-  }
-
-  button {
-    padding: 10px 24px;
-    background: #141414;
-    color: #fff;
-    border: none;
-  }
-`;
-
-// const CloseModalButton = styled(MdClose)`
-//   cursor: pointer;
-//   position: absolute;
-//   top: 20px;
-//   right: 20px;
-//   width: 32px;
-//   height: 32px;
-//   padding: 0;
-//   z-index: 10;
-// `;
-
+//  message shown when user win the game
+// could be closed by clicking in any place
 const Modal = () => {
 
   const { showModal, toggleModal } = useModalContext();
@@ -81,18 +24,27 @@ const Modal = () => {
   return (
     <>
       {
-        showModal && (field.score === field.bomb) ?
+        // modal would be shown ust when game over and user win
+        showModal && (field.score === field.bomb)
+          ?
           (
             <Background onClick={() => closeModal()}>
-              <ModalWrapper>
-                <Loader />
-                <Spinner />
-                <Loader />
-                <Spinner />
-                <ModalContent>
-                  <div className="game-over">Все бомбы найдены</div>
-                </ModalContent>
-              </ModalWrapper>
+              <Spring
+                from={{ opacity: 0, transform: `translateY(-300%)` }}
+                to={{ opacity: 1, transform: `translateY(0%)` }}
+                config={{ duration: 550 }}>
+                {props => (
+                  <ModalWrapper style={props}>
+                    <Loader />
+                    <Spinner />
+                    <Loader />
+                    <Spinner />
+                    <ModalContent>
+                      <WinText className="game-over" >Все бомбы найдены</WinText>
+                    </ModalContent>
+                  </ModalWrapper>
+                )}
+              </Spring>
             </Background>
           ) : null
       }
---
+++ src/components/Modal/ModalElements.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+export const Background = styled.div`
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.8);
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ModalWrapper = styled.div`
+  box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+  background: #fff;
+  color: #000;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  position: relative;
+  z-index: 10;
+  border-radius: 10px;
+`;
+
+export const ModalContent = styled.div`
+  grid-column: 2 /4;
+  justify-content: center;
+  align-items: center;
+  line-height: 1.8;
+  color: #141414;
+
+  p {
+    margin-bottom: 1rem;
+  }
+
+  button {
+    padding: 10px 24px;
+    background: #141414;
+    color: #fff;
+    border: none;
+  }
+`;
+
+export const WinText = styled.div`
+  margin: 1rem 0.3rem;
+  color: #73a839;
+  font-weight: 700;
+`
