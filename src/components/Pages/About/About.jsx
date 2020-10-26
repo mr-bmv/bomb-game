8097848a6c573ec2cb95122fe6ee8ea5432be8e8
mr-bmv <mr.m.botkin@gmail.com>
2020-10-26 23:28:00 +0300

    BG Add animation to About and Rules pages

--- src/components/Pages/About/About.jsx
+++ src/components/Pages/About/About.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
+import { Spring } from 'react-spring/renderprops';
 import './About.css'
 
 const About = () => {
     return (
-        <div className="about-wrapper">
-            <h2>Обо мне</h2>
-            <div className="card"><p>Меня зовут <span style={{ color: '#73a839' }}>Михаил Боткин</span></p>
-                <p>Я работал как <span style={{ color: '#73a839' }}>Software Developer </span>три года и теперь решил попробовать себя в Web разработке.</p>
-                <p>Это приложение - мой первый самостоятельный проект.</p>
-            </div>
-        </div>
+        <Spring
+            from={{ opacity: 0, transform: `translateY(30%)` }}
+            to={{ opacity: 1, transform: `translateY(0%)` }}
+            config={{ duration: 500 }}>
+            {props => (
+                <div className="about-wrapper" style={props}>
+                    <h2>Обо мне</h2>
+                    <div className="card"><p>Меня зовут <span style={{ color: '#73a839' }}>Михаил Боткин</span></p>
+                        <p>Я работал как <span style={{ color: '#73a839' }}>Software Developer </span>три года и теперь решил попробовать себя в Web разработке.</p>
+                        <p>Это приложение - мой первый самостоятельный проект.</p>
+                    </div>
+                </div>
+            )}
+        </Spring>
     )
 }
 
